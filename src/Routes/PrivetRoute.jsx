fefe0853af0659cf2e3fea5e958af04c18585f1a
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-04-29 05:39:26 +0600

    Added loading animation for loading time

--- src/Routes/PrivetRoute.jsx
+++ src/Routes/PrivetRoute.jsx
@@ -1,11 +1,18 @@
 import { useContext } from "react";
+import Lottie from "lottie-react";
 import { AuthContext } from "../AuthContextProvider/ContextProvider";
 import { Navigate, useLocation } from "react-router-dom";
 import PropTypes from "prop-types";
+import lottie3 from "../../public/Lottie/loading.json"
 
 const PrivetRoute = ({ children }) => {
-  const { user } = useContext(AuthContext);
+  const { user,loading } = useContext(AuthContext);
   const location = useLocation();
+
+  if(loading){
+    return <div className="min-h-screen  flex items-center justify-center"><Lottie style={{ width: "300px" }} animationData={lottie3}></Lottie></div>
+  }
+
   if (user) {
     return children;
   }
